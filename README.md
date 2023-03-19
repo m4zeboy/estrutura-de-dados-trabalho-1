a879096fecc5666ad0d812931483fd0340cfee5e
m4zeboy <moises775azevedo@gmail.com>
2023-03-18 21:38:10 -0300

    Funções de remoção e abstração das funções para um arquivo de cabeçalho

--- README.md
+++ README.md
@@ -1,20 +1,21 @@
 ## Dificuldades
 **16/03/203**
-
 Encontrei dificuldades em alocar memoria dinamicamente para 'strings'. 
 Primeiramente nem me dei conta de que deveria alocar memória para uma cadeia, depois fiquei travado no código abaixo.
-O codigo não funcionava, pois, eu colocava a função sizeof e esquecia de informar uma posição a mais para o caractere nulo: 
-
+O código não funcionava, pois, eu colocava a função sizeof e esquecia de informar uma posição a mais para o carácter nulo: 
 ```c
 new->data = (char *) malloc(sizeof(strlen("string2")))
 /* depois de muita dor de cabeça funcionou assim */
 new->data = (char *) malloc(strlen("string2") + 1)
 ```
-Esse código é usado para inserir uma palavra na lista, por causa desse erro pensei que a lógica da função de inserção estava errada, ou os ponteiros e perdi um tempo consideravel, mas acredito que foi válido, pois eu realmente não sabia trabalhar dessa forma com 'strings'.
+Esse código é usado para inserir uma palavra na lista, por causa desse erro pensei que a lógica da função de inserção estava errada, ou os ponteiros e perdi um tempo considerável, mas acredito que foi válido, pois eu realmente não sabia trabalhar dessa forma com 'strings'.
 
-Também enrolei-me para liberar a memória de uma lista, eu esquecia de salvar o próximo nó da lista, e quando eu tentava reatribuir o nó atual ele não funcionava por que eu liberei ele, logo o programa não ia conseguir acessar para fazer a reatribuição.
+Também enrolei-me para liberar a memória de uma lista, eu esquecia de salvar o próximo nó da lista, e quando eu tentava re-atribuir o nó atual ele não funcionava por que eu liberei ele, logo o programa não ia conseguir acessar para fazer a re-atribuição.
 
-Tive dificuldade em como tratar a função remove, quando recebe uma entrada e quando recebe duas entradas, resolvi pesquisando na internet, creditos: ChatGPT;
+Tive dificuldade em como tratar a função remove, quando recebe uma entrada e quando recebe duas entradas, resolvi essa questão pesquisando na internet, onde encontrei sobre a função *fgets* e *sscanf*. créditos: ChatGPT;
+
+## Problemas do programa
+Se ao inserir um par de palavras (str1,str2) acontecer uma colisão com a str1, não será criada uma lista para a str1 e seus sinónimos, mas se no próximo passo da função, ao inserir o outro par (str2, str1) não acontecer nenhuma colisão, a lista será criada normalmente e a str1 estará presente como um sinonimo da str2.
 
 - [x]  função de insere
     - [x] funçao hash
@@ -24,4 +25,12 @@ Tive dificuldade em como tratar a função remove, quando recebe uma entrada e q
 - [x] salvar dados em arquivo
 - [x] carregar dados do arquivo
 - [x] função de busca
-- [ ] função de remoção
+- [] função de remoção
+    - [ ] se o sinonimo str2 a ser removido for o unico sinonimo da palavra str1, remover a lista str1 da estrutura.
+- [ ] Relatório
+    - [ ] Como estão estruturados os dados
+    - [ ] Justificativa para o tamanho da tabela
+    - [ ] Justificativa para a escolha da função de dispersão
+    - [ ] Quais partes foram desenvolvidas com sucesso
+    - [ ] Quais partes não funcionam
+    - [ ] Principais dificuldades
---
+++ hash.h
@@ -0,0 +1,266 @@
+/******************************************************
+*                                                     *
+*   Moisés Silva de Azevedo                           *
+*   RGA 2022.0743.004-6                               *
+*   Implementação 1                                   *
+*   Disciplina: Estruturas de Dados e Programacão I   *
+*   Professor: Ronaldo Fiorilo *                      *
+*                                                     *
+*******************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TABLE_LENGTH 577
+#define STRING_MAX_LENGTH 30
+
+/* Estrutura para armazenar uma palavra */
+typedef struct word {
+    char *data;
+    struct word *next;
+} word;
+
+/* Dado x e n calcula o valor de x elevado a n */
+int power(int x, int n) {
+  if(n == 0) {
+    return 1;
+  } else if (n == 1) {
+    return x;
+  } else
+    return x * power(x, n - 1);
+}
+
+/* Dado uma string, calcula o hash dessa string */
+unsigned int hash(char *str) {
+  int i, k;
+  unsigned long count = 0;
+  k = 13;
+  for(i = 0; str[i] != '\0'; i++) {
+    count = count + (str[i] * power(k, i));
+  }
+  return count % TABLE_LENGTH;
+}
+
+/* Aloca memória para a criação de uma palavra */
+word *newWord(char *data) {
+  word *new = (word *) malloc(sizeof(word));
+  if(new) {
+    new->data = (char *) malloc(strlen(data) + 1);
+    if(new->data) {
+      strcpy(new->data, data);
+    }
+    new->next = NULL;
+  }
+  return new;
+}
+
+/* Recebe a cabeça de uma lista e uma chave e a insere na lista em ordem */
+void insertInList(word **head, char *data) {
+  word *new = newWord(data);
+  if(new) {
+    word *p, *q;
+    p = *head;
+    q = (*head)->next;
+    while(q && strcmp(data, q->data) > 0) {
+      p = q;
+      q = q->next;
+    }
+    /* insere se a palavra não for repetida */
+    if(q == NULL || strcmp(data, q->data) != 0) {
+      new->next = q;
+      p->next = new;
+    }
+  }
+}
+
+void removeFromList(word *head, char *str) {
+  word *p, *q;
+  p = head;
+  q = head->next;
+  while(q && strcmp(q->data, str) != 0) {
+    p = q;
+    q = q->next;
+  }
+  p->next = q->next;
+  free(q->data);
+  free(q);
+}
+
+/* Recebe a cabeça de uma lista e imprime na tela seus dados */
+void showList(word *head) {
+  while(head != NULL) {
+    printf("%s - %x -> ", head->data, head->next);
+    head = head->next;
+  }
+}
+
+/* Inicializa todas as posições da tabela com o ponteiro NULL */
+void initTable(word *table[]) {
+  int i;
+  for (i = 0; i < TABLE_LENGTH; i++){
+    table[i] = NULL;
+  }
+}
+
+/* Recebe a tabela e uma chave e cria uma palavra nova e coloca na estrutura caso não aconteça colisões */
+void insert(word *table[], char *str1, char *str2) {
+  int index = hash(str1);
+  word *head;
+  if(table[index] == NULL) {
+    /* a str1 não existe na estrutura */
+    word *new = newWord(str1);
+    table[index] = new;
+    insertInList(&table[index], str2);
+  } else if(strcmp(table[index]->data, str1) == 0) {
+    /* a str1 já existe na estrutura */
+    head = table[index];
+    insertInList(&head, str2);
+  } else {
+    fprintf(stderr, "Erro: Nao foi possível inserir a palavra '%s' na estrutura devido a uma colisao.\n", str1);
+  }
+}
+
+/* Conta quantas linhas da tabelas estão ocupadas */
+int countNotEmptyRows(word *table[]) {
+  int count, i;
+  count = 0;
+  for(i = 0; i < TABLE_LENGTH; i++) {
+    if(table[i] != NULL) {
+      count++;
+    }
+  }
+  return count;
+}
+
+/* Imprime a tabela */
+void showTable(word *table[]) {
+  int i;
+  word *current;
+  for(i = 0; i < TABLE_LENGTH; i++) {
+    if(table[i] == NULL) {
+      printf("NULL");
+    } else {
+      printf("head: %s -> ", table[i]->data);
+      current = table[i]->next;
+      while(current) {
+        printf("%s -> ",current->data);
+        current = current->next;
+      }
+    }
+    printf("\n");
+  }
+}
+
+/* Recebe a cabeça de uma lista e calcula seu tamanho */
+int listLength(word *head) {
+  int count = 0;
+  while(head) {
+    count++;
+    head = head->next;
+  }
+  return count;
+}
+
+/* Recebe a tabela de dispersão e salva seus dados em um arquivo */
+void saveTableInFile(word *table[]) {
+  FILE *file;
+  file = fopen("words.txt","w");
+  if(file) {
+    int i;
+    /* Imprime a quantidade de linhas usadas da tabela. Será usado posteriormente para a leitura correta */
+    fprintf(file, "%d\n", countNotEmptyRows(table));
+    for(i = 0; i < TABLE_LENGTH; i++) {
+      if(table[i]) {
+        int list_length;
+        word *temp = table[i];
+        list_length = listLength(temp);
+        /* Imprime o tamanho da lista atual será usado posteriormente para o programa fazer a leitura correta */
+        fprintf(file,"%d\n", list_length);
+        while(temp) {
+          fprintf(file, "%s\n", temp->data);
+          temp = temp->next;
+        }
+      }
+    }
+    fclose(file);
+  }
+}
+
+/* Carrega os dados de um arquivo e armazena em uma tabela de dispersão */
+void loadTableFromFile(word *table[]) {
+  FILE *file;
+  file = fopen("words.txt", "r");
+  if(file) {
+    int count_not_empty_rows, i;
+    fscanf(file, "%d\n", &count_not_empty_rows);
+    for (i = 0; i < count_not_empty_rows; i++) {
+      int list_length, j;
+      fscanf(file,"%d\n", &list_length);
+      for(j = 0; j < list_length; j++) {
+        int index;
+        char str[STRING_MAX_LENGTH];
+        fscanf(file, "%s\n", str);
+        if(j == 0) {
+          /* É a cabeça da lista, a palavra chave */
+          word *head;
+          head = newWord(str);
+          index = hash(str);
+          table[index] = head;
+        } else {
+          insertInList(&table[index], str);
+        }
+
+      }
+    }
+    fclose(file);
+  } else {
+    fprintf(stderr, "Erro ao abriro o arquivo 'words.txt'.\n");
+  }
+}
+/* Dada uma lista, libera a memoria de cada nó */
+void freeList(word *head) {
+  word *current;
+  current = head;
+  while(current) {
+    word *next = current->next;
+    free(current->data);
+    free(current);
+    current = next;
+  }
+}
+
+/* Percorre as linhas da tabela e se estiver vazia, libera a memória da lista que está alocada na respectiva posição */
+void freeTable(word *table[]) {
+  int i;
+  for(i = 0; i < TABLE_LENGTH; i++) {
+    if(table[i] != NULL) {
+      freeList(table[i]);
+    }
+  }
+}
+
+/* Recebe uma palavra e verifica se ela se encontra na estrutura, se sim mostra seus sinonimos um em cada linha */
+void search(word *table[], char *str) {
+    int index;
+    index = hash(str);
+    if(table[index] != NULL && strcmp(str, table[index]->data) == 0) {
+      word *iterator = table[index]->next;
+      while(iterator) {
+        printf("%s\n", iterator->data);
+        iterator = iterator->next;
+      }
+    } else {
+      printf("hein?\n");
+    }
+}
+
+/* Dado uma palavra str exclui ela e sua lista de sinônimos da estrutura, mas não exclui a palavra str como sinonimo em outras listas */
+void removeFromTable(word *table[], char *str) {
+  int index;
+  word *head;
+  index = hash(str);
+  head = table[index];
+  freeList(head);
+  table[index] = NULL;
+}
