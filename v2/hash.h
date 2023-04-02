d5903b4fca8d373735e88c8cc08c9e50ef43be54
m4zeboy <moises775azevedo@gmail.com>
2023-04-02 16:30:44 -0300

    Documentando

--- v2/hash.h
+++ v2/hash.h
@@ -6,22 +6,13 @@
 #define string char *
 #define SIZE 177
 
-typedef struct synonym {
-  string data;
-  struct synonym *next;
-} Synonym;
-
-typedef struct node {
-  string word;
-  Synonym *list;
-  struct node *next;
-} Node;
-
+/* Calcula x elevado a n */
 int power(int x, int n) {
   if(n == 0) return 1;
   return x * power(x, n - 1);
 }
 
+/* Dado uma string calcula o seu índice de dispersão */
 unsigned int hash(string data) {
   int k, i;
   unsigned int hashValue = 0;
@@ -32,30 +23,13 @@ unsigned int hash(string data) {
   return hashValue % SIZE;
 }
 
-Node *createNode(string data) {
-  Node *new;
-  new = (Node *) malloc(sizeof(Node));
-  if(new == NULL) {
-    return NULL;
-  }
-  new->word = (string) malloc(strlen(data) + 1);
-  if(new->word == NULL) {
-    return NULL;
-  }
-  strcpy(new->word, data);
-  new->list = NULL;
-  new->next = NULL;
-  return new;
-}
+/* Tipo do nó de sinônimo */
+typedef struct synonym {
+  string data;
+  struct synonym *next;
+} Synonym;
 
-unsigned int nodeLength(Node *list) {
-  if(list == NULL) {
-    return 0;
-  } else {
-    return 1 + nodeLength(list->next);
-  }
-}
-
+/* Cria um sinônimo e retorna seu ponteiro */
 Synonym *createSynonym(string data) {
   Synonym *new;
   new = (Synonym *) malloc(sizeof(Synonym));
@@ -71,6 +45,7 @@ Synonym *createSynonym(string data) {
   return new;
 }
 
+/* Calcula o tamanho da lista de sinônimos */
 unsigned int synonymLength(Synonym *list) {
   if(list == NULL) {
     return 0;
@@ -79,6 +54,7 @@ unsigned int synonymLength(Synonym *list) {
   }
 }
 
+/* Insere um sinônimo em uma lista ordenada */
 bool insertSynonymInList(Synonym **list, string synonym) {
   Synonym *new, *previous, *current;
   previous = NULL;
@@ -100,6 +76,185 @@ bool insertSynonymInList(Synonym **list, string synonym) {
   return true;
 }
 
+/* Remove um sinônimo de uma lista */
+void deleteSynonym(Synonym **list, string synonym) {
+  Synonym *previous, *current, *trash;
+  previous = NULL;
+  current = *list;
+  while(current && strcmp(synonym, current->data) != 0) {
+    previous = current;
+    current = current->next;
+  }
+  trash = current;
+  if(previous == NULL) {
+    *list = current->next;
+  } else {
+    trash = current;
+    previous->next = current->next;
+  }
+  free(trash->data);
+  free(trash);
+}
+
+/* Mostra a lista de sinônimos um em cada linha*/
+void showSynonymList(Synonym *list) {
+  while (list)  {
+    printf("%s\n", list->data);
+    list = list->next;
+  }
+  
+}
+
+/* Libera a memória alocada para a lista de sinônimos */
+void freeSynonymList(Synonym *list) {
+  Synonym *next;
+  while(list) {
+    next = list->next;
+    free(list->data);
+    free(list);
+    list = next;
+  }
+}
+
+/* Escreve no arquivo um sinônimo */
+void saveSynonym(FILE *stream, Synonym *s) {
+  fprintf(stream, "%s\n", s->data);
+}
+
+/* Escreve no arquivo a lista de sinônimos */
+void saveSynonymList(FILE *stream, Synonym *list) {
+  while(list) {
+    saveSynonym(stream, list);
+    list = list->next;
+  }
+}
+
+/* Lê a lista de sinônimos do arquivo */
+void readSynonymList(FILE *stream, int synonyms, Synonym **list) {
+  int k;
+  for(k = 0; k < synonyms; k++) {
+    char synonym[30];
+    fscanf(stream,"%s\n", synonym);
+    insertSynonymInList(list, synonym);
+  }
+}
+
+/* Tipo de um nó */
+typedef struct node {
+  string word;
+  Synonym *list;
+  struct node *next;
+} Node;
+
+/* Cria um nó e devolve o ponteiro */
+Node *createNode(string data) {
+  Node *new;
+  new = (Node *) malloc(sizeof(Node));
+  if(new == NULL) {
+    return NULL;
+  }
+  new->word = (string) malloc(strlen(data) + 1);
+  if(new->word == NULL) {
+    return NULL;
+  }
+  strcpy(new->word, data);
+  new->list = NULL;
+  new->next = NULL;
+  return new;
+}
+
+/* Calcula o tamanho da lista */
+unsigned int nodeLength(Node *list) {
+  if(list == NULL) {
+    return 0;
+  } else {
+    return 1 + nodeLength(list->next);
+  }
+}
+
+/* Busca a palavra na lista e remove seu nó */
+void deleteNode(Node **list, string word) {
+  Node *previous, *current, *trash;
+  previous = NULL;
+  current = *list;
+  while(current && strcmp(word, current->word) != 0) {
+    previous = current;
+    current = current->next;
+  }
+  if(strcmp(current->word, word) == 0 && current != NULL) {
+    trash = current;
+    if(previous == NULL) {
+      *list = current->next;
+    } else {
+      previous->next = current->next;
+    }
+    free(trash->word);
+    freeSynonymList(trash->list);
+    free(trash);
+  } else {
+    printf("hein?\n");
+  }
+}
+
+/* Libera memória alocada para a lista */
+void freeNodeList(Node *list) {
+  while (list) {
+    Node *next = list->next;
+    free(list->word);
+    freeSynonymList(list->list);
+    free(list);
+    list = next;
+  }
+}
+
+/* Escreve um nó no arquivo */
+void saveNode(FILE *stream, Node *n) {
+  Synonym *s;
+  unsigned int synonyms;
+  s = n->list;
+  synonyms = synonymLength(s);
+  fprintf(stream, "%s\n", n->word);
+  fprintf(stream, "%d\n", synonyms);
+  saveSynonymList(stream, s);
+}
+
+/* Escreve uma lista de nós no arquivo */
+void saveNodeList(FILE *stream, Node *n) {
+  unsigned int nodes;
+  Node *iterator = n;
+  nodes = nodeLength(n);
+  fprintf(stream,"%d\n", nodes);
+  while(iterator) {
+    saveNode(stream, iterator);
+    iterator = iterator->next;
+  }
+}
+
+/* Lê uma lista de nós do arquivo */
+void readNodeList(FILE *stream, int nodes, Node **nodeList, int *baseAddress) {
+  int j;
+  for(j = 0; j < nodes; j++) {
+    char word[30];
+    int synonyms;
+    Node *node;
+    Synonym *list;
+    fscanf(stream, "%s\n", word);
+    fscanf(stream,"%d\n", &synonyms);
+    list = NULL;
+    readSynonymList(stream, synonyms, &list);
+    node = createNode(word);
+    node->list = list;
+    if(j == 0) {
+      *baseAddress = hash(word);
+      *nodeList = node;
+    } else {
+      node->next = (*nodeList)->next;
+      (*nodeList)->next = node;
+    }
+  }
+}
+
+/* Inicia a tabela com NULL em todas as posições */
 void initTable(Node *table[]) {
   int i;
   for(i = 0; i < SIZE; i++) {
@@ -107,6 +262,7 @@ void initTable(Node *table[]) {
   }
 }
 
+/* Tenta inserir uma palavra e um sinônino na tabela, caso falhe a função retorna false */
 bool insertInTable(Node *table[], string word, string synonym) {
   int baseAddress;
   baseAddress = hash(word);
@@ -132,13 +288,7 @@ bool insertInTable(Node *table[], string word, string synonym) {
   }
 }
 
-void showSynonymList(Synonym *list) {
-  while(list) {
-    printf("%s\n", list->data);
-    list = list->next;
-  }
-}
-
+/* Busca por uma palavra na tabela */
 void search(Node *table[], string word) {
   int baseAddress;
   Node *iterator;
@@ -155,24 +305,7 @@ void search(Node *table[], string word) {
   }
 }
 
-void deleteNode(Node **list, string word) {
-  Node *previous, *current, *trash;
-  previous = NULL;
-  current = *list;
-  while(current && strcmp(word, current->word) != 0) {
-    previous = current;
-    current = current->next;
-  }
-  trash = current;
-  if(previous == NULL) {
-    *list = current->next;
-  } else {
-    previous->next = current->next;
-  }
-  free(trash->word);
-  free(trash);
-}
-
+/* Calcula quantas linhas foram usadas na tabela */
 unsigned int countUsedRows(Node *table[]) {
   unsigned int i, count;
   count = 0;
@@ -184,57 +317,7 @@ unsigned int countUsedRows(Node *table[]) {
   return count;
 }
 
-void showNodeList(Node *list) {
-  while(list) {
-    printf("%s -> ", list->word);
-    list = list->next;
-  }
-}
-
-void showTable(Node *table[]) {
-  int i;
-  for(i = 0; i < SIZE; i ++) {
-    printf("| %02d |", i);
-    if(table[i] == NULL) printf(" NULL |\n");
-    else {
-      showNodeList(table[i]);
-      printf("\n");
-    }
-  }
-}
-
-void saveSynonym(FILE *stream, Synonym *s) {
-  fprintf(stream, "%s\n", s->data);
-}
-
-void saveSynonymList(FILE *stream, Synonym *list) {
-  while(list) {
-    saveSynonym(stream, list);
-    list = list->next;
-  }
-}
-
-void saveNode(FILE *stream, Node *n) {
-  Synonym *s;
-  unsigned int synonyms;
-  s = n->list;
-  synonyms = synonymLength(s);
-  fprintf(stream, "%s\n", n->word);
-  fprintf(stream, "%d\n", synonyms);
-  saveSynonymList(stream, s);
-}
-
-void saveNodeList(FILE *stream, Node *n) {
-  unsigned int nodes;
-  Node *iterator = n;
-  nodes = nodeLength(n);
-  fprintf(stream,"%d\n", nodes);
-  while(iterator) {
-    saveNode(stream, iterator);
-    iterator = iterator->next;
-  }
-}
-
+/* Salva a tabela no arquivo */
 void save(Node *table[]) {
   FILE *file;
   file = fopen("data.data", "w");
@@ -251,38 +334,7 @@ void save(Node *table[]) {
   }
 }
 
-void readSynonymList(FILE *stream, int synonyms, Synonym **list) {
-  int k;
-  for(k = 0; k < synonyms; k++) {
-    char synonym[30];
-    fscanf(stream,"%s\n", synonym);
-    insertSynonymInList(list, synonym);
-  }
-}
-
-void readNodeList(FILE *stream, int nodes, Node **nodeList, int *baseAddress) {
-  int j;
-  for(j = 0; j < nodes; j++) {
-    char word[30];
-    int synonyms;
-    Node *node;
-    Synonym *list;
-    fscanf(stream, "%s\n", word);
-    fscanf(stream,"%d\n", &synonyms);
-    list = NULL;
-    readSynonymList(stream, synonyms, &list);
-    node = createNode(word);
-    node->list = list;
-    if(j == 0) {
-      *baseAddress = hash(word);
-      *nodeList = node;
-    } else {
-      node->next = (*nodeList)->next;
-      (*nodeList)->next = node;
-    }
-  }
-}
-
+/* Lê a tabela do arquivo */
 void read(Node *table[]) {
   FILE *file;
   file = fopen("data.data", "r");
@@ -297,5 +349,15 @@ void read(Node *table[]) {
       table[baseAddress] = nodeList;
     }
     fclose(file);
+  }
+}
+
+/* Libera a memória alocada para a tabela */
+void freeTable(Node *table[]) {
+  int i;
+  for (i = 0; i < SIZE; i++) {
+    if(table[i]) {
+      freeNodeList(table[i]);
+    }
   }
 }
--- v2/index.c
+++ v2/index.c
@@ -6,7 +6,6 @@
 
 int main(void) {
   Node *table[SIZE];
-  int inputCount;
   char input[73], op[12], word[30], synonym[30];
   initTable(table);
   read(table);
@@ -24,13 +23,42 @@ int main(void) {
     } else if(strcmp(op,"remove") == 0) {
       if(inputCount == 2) {
         /* Remover a palavra str1 e sua lista de sinonimos da estrutura */
-        deleteNode(&(table[hash(word)]), word);
+        int index = hash(word);
+        if(table[index] == NULL) {
+          printf("hein?\n");
+        } else {
+          deleteNode(&(table[index]), word);
+        }
       } else if(inputCount == 3) {
         /* Remover str2 da lista de sinonimos de str1, e remover str1 da lista de sinonimos de str2 */
-        int index = hash(word);
-        /* Verifica se a palavra não tem mais sinonimos associados a ela para excluir da tabela */
-        if(table[index]->next == NULL) {
+        int index;
+        Node *previous, *current, *trash;
+        index = hash(word);
+        previous = NULL;
+        current = table[index];
+        while(current && strcmp(current->word, word) != 0) {
+          previous = current;
+          current = current->next;
+        };
+        if(current) {
+          deleteSynonym(&(current->list), synonym);
+          if(current->list == NULL) {
+          if(previous == NULL) {
+            trash = current;
+            table[index] = current->next;
+            free(trash->word);
+            free(trash);
+          } else {
+            trash = current;
+            previous->next = current->next;
+            free(trash->word);
+            free(trash);
+          }
         }
+        } else {
+          printf("hein?\n");
+        }
+      
       } 
     } else if(strcmp(op, "fim") == 0) {
       break;
@@ -39,5 +67,6 @@ int main(void) {
 
 
   save(table);
+  freeTable(table);
   return 0;
 }
